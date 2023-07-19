07aad6d78df845aabdfbf93965a3a18ecf91d8c8
Maximus-22 <maksim.melnichenko@nservice.com.ua>
2023-07-20 02:03:39 +0300

    Change version after bagfix
    version = "1.0.1"

--- clean_folder_mmv22/README.md
+++ clean_folder_mmv22/README.md
@@ -3,3 +3,4 @@
 This package implements a script that sorts files in a specified folder.
 The script checks the file extension and, depending on the extension,
 creates a new directory and transfers this file to the appropriate category.
+The script needs a command line parameter - target directory.
--- clean_folder_mmv22/setup.py
+++ clean_folder_mmv22/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 
 setup(name = "clean_folder_mmv22",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "Sorting and transfer files in a specified folder",
     author = "maximus_22",
     author_email = "maksim.melnichenko@nservice.com.ua",
